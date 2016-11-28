c6057563953676d46c89de91f1c1df55de545e36
jerson morgado hernandez <jerson morgado hernandez>
2016-11-28 17:48:31 -0600

    cambios 05:48

--- application/views/OrdenesCom.php
+++ application/views/OrdenesCom.php
@@ -4,48 +4,61 @@
 <head>
     <title>disabled-checkbox</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/bootstrap-table/src/bootstrap-table.css">
-    <link rel="stylesheet" href="../assets/examples.css">
-    <script src="../assets/jquery.min.js"></script>
-    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../assets/bootstrap-table/src/bootstrap-table.js"></script>
-    <script src="../ga.js"></script>
+    
+    <link rel="stylesheet" href="//localhost/intranet/assets/bootstrap-table/src/bootstrap-table.css">
+    <link rel="stylesheet" href="//localhost/intranet/assets/examples.css">
+    <script src="//localhost/intranet/assets/jquery.min.js"></script>
+    <script src="//localhost/intranet/assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="//localhost/intranet/assets/bootstrap-table/src/bootstrap-table.js"></script>
+    <script src="//localhost/intranet/ga.js"></script>
 </head>
 <body>
     <div class="container">
-        <h1>Disabled Checkbox</h1>
+        <h3>ORDENES DE COMPRA POR AUTORIZAR</h3>
         <p>Use <code>checkboxHeader</code>, <code>checkboxEnable</code> options and <code>formatter</code> column option to disabled select input.</p>
         <table id="table"
                data-toggle="table"
-               data-height="460"
+               data-height="420"
                data-click-to-select="true"
-               data-url="../json/data1.json">
+               data-detail-view="true"
+               data-detail-formatter="detailFormatter"
+               data-url="//localhost/table-examples/json/data1.json">
             <thead>
             <tr>
-                <th data-field="state" data-checkbox="true"
-                    data-formatter="stateFormatter"></th>
+
                 <th data-field="id">ID</th>
                 <th data-field="name">Item Name</th>
                 <th data-field="price">Item Price</th>
+                <th data-field="state" data-checkbox="true"
+                data-formatter="stateFormatter"></th>
             </tr>
             </thead>
         </table>
     </div>
 <script>
     function stateFormatter(value, row, index) {
-        if (index === 2) {
+        if (index === 1000) {
             return {
                 disabled: true
             };
         }
-        if (index === 5) {
+        if (index === 1001) {
             return {
                 disabled: true,
                 checked: true
             }
         }
         return value;
+    }
+    
+</script>
+<script>
+    function detailFormatter(index, row) {
+        var html = [];
+        $.each(row, function (key, value) {
+            html.push('<p><b>' + key + ':</b> ' + value + '</p>');
+        });
+        return html.join('');
     }
 </script>
 </body>
