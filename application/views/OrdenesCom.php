5a3fc91db4bd5439fd68a0d09bc2b1e3bbf5f484
jerson morgado hernandez <jerson morgado hernandez>
2016-11-30 17:21:51 -0600

    cambios 05:21

--- application/views/OrdenesCom.php
+++ application/views/OrdenesCom.php
@@ -31,7 +31,7 @@
                
             <thead>
             <tr>  
-                <th data-field="OrdenComp" data-switchable="false">OrdenComp</th>
+                <th data-field="OrdenComp" data-switchable="false">OrdenCompra</th>
                 <th data-field="ClaveProv"data-visible="false">ClaveProv</th>
                 <th data-field="ClaveMon"data-visible="false">ClaveMon</th>
                 <th data-field="DescMon" data-visible="false">DescMon</th>
@@ -61,18 +61,17 @@
                 <th data-field="CveComp"data-visible="false">CveComp</th>
                 <th data-field="NumReq"data-visible="false">NumReq</th>
                 <th data-field="state" data-checkbox="true"
-                data-formatter="stateFormatter"></th>
+                    data-formatter="stateFormatter"></th>
             </tr>
             
             </thead>
             <?php  
-
+ 
       $db_host="localhost";
       $db_user="root";
       $db_password= "";
       $db_name="localicom";
       $db_table_name="detallescompra";
-
       $db_connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
       $sql = "SELECT OrdenComp, ClaveProv, ClaveMon, DescMon, TipCamb, NomProv, Cve_iva, ClaveProd, Unidad, DescProd, StatusPart, FaltaPed, FechEnt, CeCo, ValorProd, dcto1, CantProd, Importe, ivaProv, Total, Partida, VistBueno, Retencion, Reten_iva, CveSuc, iepsProd, DesClase, CveComp, NumReq FROM detallescompra";
       $resultado = mysqli_query($db_connection,$sql);
@@ -89,7 +88,7 @@
 	        $row["DescMon"] . "</font></td>";
         echo "<td width=\"25%\"><font face=\"verdana\">" .
 	        $row["TipCamb"] . "</font></td>";
-        echo "<td width=\"8%\"><font face=\"verdana\">$" .
+        echo "<td width=\"8%\"><font face=\"verdana\">" .
 	        $row["NomProv"] . "</font></td>";
         echo "<td width=\"6%\"><font face=\"verdana\">" .
 	        $row["Cve_iva"] . "</font></td>";
@@ -101,13 +100,13 @@
 	        $row["DescProd"] . "</font></td>";
         echo "<td width=\"25%\"><font face=\"verdana\">" .
 	        $row["StatusPart"] . "</font></td>";
-        echo "<td width=\"8%\"><font face=\"verdana\">$" .
+        echo "<td width=\"8%\"><font face=\"verdana\">" .
 	        $row["FaltaPed"] . "</font></td>";
         echo "<td width=\"6%\"><font face=\"verdana\">" .
 	        $row["FechEnt"] . "</font></td>";
-        echo "<td width=\"25%\"><font face=\"verdana\">" .
-	        $row["CeCo"] . "</font></td>";
-        echo "<td width=\"8%\"><font face=\"verdana\">$" .
+        echo utf8_encode("<td width=\"25%\"><font face=\"verdana\">" .
+	        $row["CeCo"] . "</font></td>");
+        echo "<td width=\"8%\"><font face=\"verdana\">" .
 	        $row["ValorProd"] . "</font></td>";
         echo "<td width=\"6%\"><font face=\"verdana\">" .
 	        $row["dcto1"] . "</font></td>";
@@ -119,23 +118,23 @@
 	        $row["ivaProv"] . "</font></td>";
         echo "<td width=\"25%\"><font face=\"verdana\">" .
 	        $row["Total"] . "</font></td>";
-        echo "<td width=\"8%\"><font face=\"verdana\">$" .
+        echo "<td width=\"8%\"><font face=\"verdana\">" .
 	        $row["Partida"] . "</font></td>";
         echo "<td width=\"6%\"><font face=\"verdana\">" .
 	        $row["VistBueno"] . "</font></td>"; 
         echo "<td width=\"25%\"><font face=\"verdana\">" .
 	        $row["Retencion"] . "</font></td>";
-        echo "<td width=\"8%\"><font face=\"verdana\">$" .
+        echo "<td width=\"8%\"><font face=\"verdana\">" .
 	        $row["Reten_iva"] . "</font></td>";
-        echo "<td width=\"6%\"><font face=\"verdana\">" .
-	        $row["CveSuc"] . "</font></td>";
+        echo utf8_encode("<td width=\"6%\"><font face=\"verdana\">" .
+	        $row["CveSuc"] . "</font></td>");
         echo "<td width=\"15%\"><font face=\"verdana\">" .
 	        $row["iepsProd"] . "</font></td>";
         echo "<td width=\"13%\"><font face=\"verdana\">" .
 	        $row["DesClase"] . "</font></td>";
         echo "<td width=\"20%\"><font face=\"verdana\">" .
 	        $row["CveComp"] . "</font></td>";   
-        echo "<td width=\"8%\"><font face=\"verdana\">$" .
+        echo "<td width=\"8%\"><font face=\"verdana\">" .
 	        $row["NumReq"]. "</font></td></tr>";
         
      }
@@ -150,13 +149,13 @@
 
                                 
 <script>
-    function stateFormatter(value, row, index) {
-        if (index === 0) {
+    function stateFormatter(value, $row, index) {
+        if (index === 2) {
             return {
                 disabled: true
             };
         }
-        if (index === 1) {
+        if (index === 1817) {
             return {
                 disabled: true,
                 checked: true
@@ -164,7 +163,6 @@
         }
         return value;
     }
-    
 </script>
 <!-- <style>
 body {
